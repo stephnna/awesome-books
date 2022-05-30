c21068619f5cac3288a3d5f17748280366d49d41
roseokpe <roseokpe@gmail.com>
2022-05-30 17:00:51 +0100

    updated code

--- script.js
+++ script.js
@@ -9,3 +9,9 @@
       author:'Rose',
     },
   ];
+
+  // <h1>Awesome Books</h1>
+  // <h4>book1<br>Author1<br></h4>
+  // <button type="button">Remove</button><hr>
+
+const awesomeBook = document.getElementById('awesome-book')
