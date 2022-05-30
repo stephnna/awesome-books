df4139717b67ebc3ce3f13092caa3bc3b4e9e6a7
stephnna <stephennnabuike@gmail.com>
2022-05-30 18:07:33 +0100

    Modify script file

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
     {
       title: 'Book1',
       author: 'Stephen',
+      remove: 'Remove',
     },
 
     {
@@ -12,16 +13,15 @@
   ];
   const awesomeBook = document.getElementById('awesome-book');
 
- for (let i = 0; i< books.length; i += 1) {
-   const bookContainer = Document.createElement('div');
+ for (let i = 0; i < books.length; i += 1) {
+   const bookContainer = document.createElement('div');
    awesomeBook.appendChild(bookContainer);
  }
 
 Array.from(awesomeBook.children).forEach((child, index) => {
-  child.innerHTML = `
-  <h1>Awesome Books</h1>
-  <h4> ${books[index].title}</h4>
-  <h4> ${books[index].author}</h4>
+  child.innerHTML = `  
+  <h4>${books[index].title}</h4>
+  <h4>${books[index].author}</h4>
   <button type="button">${books[index].remove}</button>
   <hr>
   `;
